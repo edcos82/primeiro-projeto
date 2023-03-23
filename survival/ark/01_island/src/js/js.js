1b3e29bf169ff77adeafebdf0d7abff2f10b980a
Edmar Gomes <120301387+edcos82@users.noreply.github.com>
2023-03-23 15:19:25 -0300

    Add files via upload

---
+++ survival/ark/01_island/src/js/js.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	
+	$(".thumbs li").click(function(){
+		var thumbs = $(this).find("img").attr("src");
+		var cover = $(".cover img");
+
+		cover.fadeTo("200", "0", function(){
+			cover.attr("src", thumbs).fadeTo("150", 1);
+		})
+
+		$(".thumbs li").remoClass("active");
+		$(this).addClass("active");
+
+	});
+
+});
